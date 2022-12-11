41f2e271a5c396db67e3a987833ded9783efefb8
Siddhant <siddhantpurohit216@gmail.com>
2022-12-11 22:32:19 +0530

    removed header from vercel.json and added cors parameter

--- index.js
+++ index.js
@@ -29,7 +29,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: origin,
+  preflightContinue: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
